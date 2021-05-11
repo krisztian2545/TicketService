b568fd4d45096c1e614fca22661d85e627fe2952
krisztian2545 <krisztian2545@gmail.com>
2021-05-11 23:06:58 +0200

    save

--- ticket-service/src/test/java/com/epam/training/ticketservice/service/impl/MovieServiceImplTest.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/service/impl/MovieServiceImplTest.java
@@ -1,24 +1,55 @@
 package com.epam.training.ticketservice.service.impl;
 
+import com.epam.training.ticketservice.dao.MovieDao;
+import com.epam.training.ticketservice.exception.AccessDeniedException;
+import com.epam.training.ticketservice.service.UserValidator;
+import com.epam.training.ticketservice.service.response.BasicCommandResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 
 class MovieServiceImplTest {
 
-    @Test
-    void createMovie() {
+    private MovieDao movieDao;
+    private UserValidator userValidator;
+    MovieServiceImpl underTest;
+
+    @BeforeEach
+    void setUp() {
+        movieDao = Mockito.mock(MovieDao.class);
+        userValidator = Mockito.mock(UserValidator.class);
+        underTest = new MovieServiceImpl(movieDao, userValidator);
     }
 
     @Test
-    void getAllMovies() {
+    void testCreateMovieReturnsSuccessfulBasicCommandResponse() {
+        BasicCommandResponse result = underTest.createMovie("doesn't", "matter", 99, "token");
+
+        assertTrue(result.isSuccessful());
     }
 
     @Test
-    void updateMovie() {
+    void testCreateMovieReturnsErrorMessageInBasicCommandResponseWhenTheUserISUnauthorized() throws AccessDeniedException {
+        doThrow(AccessDeniedException.class).when(userValidator).authorizeAdmin("invalid token");
+        BasicCommandResponse result = underTest.createMovie("doesn't", "matter", 99, "invalid token");
+
+        assertFalse(result.isSuccessful());
     }
 
     @Test
-    void deleteMovie() {
+    void testGetAllMovies() {
+    }
+
+    @Test
+    void testUpdateMovie() {
+    }
+
+    @Test
+    void testDeleteMovie() {
     }
 }
